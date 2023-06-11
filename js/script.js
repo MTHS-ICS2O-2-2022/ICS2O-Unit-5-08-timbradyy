afac56bd000b4ee0af65cac241fee15e941cb8ae
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-06-11 23:30:45 +0000

    super super done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  const dividend = parseInt(document.getElementById("dividend").value)
+  const divisor = parseInt(document.getElementById("divisor").value)
+  let remainder = dividend
+  let output = 0
+
+  while (remainder >= divisor) {
+    remainder -= divisor
+    output++
+  }
+  if (remainder > 0) {
+    document.getElementById("answer").innerHTML =
+      dividend +
+      " ÷ " +
+      divisor +
+      " = " +
+      output +
+      " with a remainder of " +
+      remainder +
+      "."
+  } else {
+    document.getElementById("answer").innerHTML =
+      dividend + " ÷ " + divisor + " = " + output + "."
+  }
+}
